bdfb50403f775e110d6df8f3451c3dc6ac6342b2
noahg09 <noah.gruenig@hispeed.ch>
2024-06-14 08:33:20 +0200

    first commit

---
+++ src/app/UserProfileForm.tsx
@@ -0,0 +1,10 @@
+import styles from '@/app/userProfileForm.module.css'
+
+export default function UserProfileForm() {
+    return (
+        <div className={styles.container}>
+        <h2 className={styles.title}>User Profile Form</h2>
+    {/* Hier können Sie Ihr Formular zur Bearbeitung des Benutzerprofils hinzufügen */}
+    </div>
+)
+}
